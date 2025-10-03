8e57463d1199e30694a61c34f2567d488a08a710
fathimafida713 <fidaaf745@gmail.com>
2025-10-03 15:59:46 +0530

    added rows

--- lib/calls.dart
+++ lib/calls.dart
@@ -5,7 +5,9 @@ class Calls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor:Colors.black,appBar: AppBar(
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.black,
         foregroundColor: Colors.white,
@@ -15,10 +17,56 @@ class Calls extends StatelessWidget {
           SizedBox(width: 20),
           Icon(Icons.more_vert),
         ],
-      ), floatingActionButton: FloatingActionButton(
+      ),
+      floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.green,
-        foregroundColor: Colors.black,child: Icon(Icons.add_call),
-      ),);
+        foregroundColor: Colors.black,
+        child: Icon(Icons.add_call),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: Text(
+              "Favorites",
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+          ),
+          SizedBox(height: 15),
+          ListTile(
+            leading: CircleAvatar(
+              child: Icon(Icons.favorite, color: Colors.black),
+              backgroundColor: Colors.green,
+            ),
+            title: Text("Add Favorite", style: TextStyle(color: Colors.white)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: Text(
+              "Recent",
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+          ),
+          SizedBox(height: 15),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/mountain.webp"),
+            ),
+            title: Text("mountain", style: TextStyle(color: Colors.white)),
+            subtitle: Row(
+              children: [
+                Icon(Icons.call_received,color: Colors.green,),
+                Text(
+                  "october 1, 11:56",
+                  style: TextStyle(color: Color.fromARGB(255, 158, 161, 163)),
+                ),
+              ],
+            ),trailing: Icon(Icons.call,color:Colors.white ,),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/chat.dart
+++ lib/chat.dart
@@ -26,28 +26,135 @@ class Chats extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          TextField(
-            style: TextStyle(color: Colors.white),
-            decoration: InputDecoration(
-              prefixIcon: Icon(
-                Icons.search,
-                color: const Color.fromARGB(255, 158, 161, 163),
-              ),
-              hintText: "Ask Meta AI or Search",
-              hintStyle: TextStyle(
-                color: const Color.fromARGB(255, 158, 161, 163),
-              ),
-
-              fillColor: const Color.fromARGB(255, 58, 58, 58),
-              filled: true,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(100),
-                borderSide: BorderSide.none,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: const Color.fromARGB(255, 158, 161, 163),
+                ),
+                hintText: "Ask Meta AI or Search",
+                hintStyle: TextStyle(
+                  color: const Color.fromARGB(255, 158, 161, 163),
+                ),
+            
+                fillColor: const Color.fromARGB(255, 58, 58, 58),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(100),
+                  borderSide: BorderSide.none,
+                ),
               ),
             ),
           ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    height: 30,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 158, 161, 163),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "All",
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 158, 161, 163),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    height: 30,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 158, 161, 163),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Unread",
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 158, 161, 163),
+                        ),
+                      ),
+                    ),
+                  ),
+            
+                  SizedBox(width: 10),
+                  Container(
+                    height: 30,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 158, 161, 163),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "favorites",
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 158, 161, 163),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    height: 30,
+                    width: 90,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: Color.fromARGB(255, 158, 161, 163),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Groups",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 158, 161, 163),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage("assets/mountain.webp"),radius: 30,
+              ),
+              title: Text("mountain", style: TextStyle(color: Colors.white)),
+              subtitle: Text(
+                "Where are you",
+                style: TextStyle(color: Color.fromARGB(255, 158, 161, 163)),
+              ),
+              trailing: Text("11:00pm",style: TextStyle(color: Color.fromARGB(255, 158, 161, 163) ),),
+            ),
+          
         ],
-      ),floatingActionButton: FloatingActionButton(
+      ),
+      floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.green,
         foregroundColor: Colors.black,
